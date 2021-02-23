34ed4eb5bedc452623d14626397a6f2ec9f239ee
prouty411 <prouty_411@hotmail.com>
2021-02-22 20:54:52 -0800

    updating

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+      <div className="masthead-image-container">
+        <img alt="Activision" src="./HomePage.css"/>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
